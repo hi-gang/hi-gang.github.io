72a51df7e6ce96497dde91113ac6a9d3765f8573
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2021-08-27 23:31:55 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1306},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-github","displayName":"Github","link":{"linkType":"LinkTypePage","href":"#!page-github"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
