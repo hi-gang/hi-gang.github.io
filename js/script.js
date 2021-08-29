d98f63aad951cdf8989c8c2d98c0c0e402d193f1
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2021-08-29 07:10:20 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1306},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-github","displayName":"Github","link":{"linkType":"LinkTypePage","href":"#!page-github"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1141},"pages":[{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-discord","displayName":"Discord","link":{"linkType":"LinkTypePage","href":"#!page-discord"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-github","displayName":"Github","link":{"linkType":"LinkTypePage","href":"#!page-github"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
